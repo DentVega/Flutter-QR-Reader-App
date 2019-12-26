1d1faa04c73ef1bb38e93019ece721f6f01c8a17
DentVega <dentvega6@gmail.com>
2019-12-25 21:41:44 -0400

    BottonNavigationBar

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,12 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:qrreaderapp/src/pages/direcciones_page.dart';
+import 'package:qrreaderapp/src/pages/mapas_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('HOLA HOME PAGE'),
-      ),
+      body: _callPage(currentIndex),
+      bottomNavigationBar: _crearBottomNavitaion(),
     );
+  }
+
+  Widget _crearBottomNavitaion() {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: (index) {
+        setState(() {
+          currentIndex = index;
+        });
+      },
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.map), title: Text('Mapa')),
+        BottomNavigationBarItem(icon: Icon(Icons.brightness_5), title: Text('Direcciones')),
+      ],
+    );
+  }
+
+  Widget _callPage(int paginaActual) {
+    switch(paginaActual) {
+      case 0: return MapasPage();
+      case 1: return DireccionesPage();
+
+      default:
+        return MapasPage();
+    }
   }
 }
