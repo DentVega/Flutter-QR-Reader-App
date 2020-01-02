91272ca4835e5cfa4c3d2098d81e54c6346ac4b0
Brian Dennis Vega Hidalgo <dentvega6@gmail.com>
2020-01-01 23:14:27 -0400

    Crear un mapa en flutter

--- lib/src/pages/mapa_page.dart
+++ lib/src/pages/mapa_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
 import 'package:qrreaderapp/src/models/scan_model.dart';
 
 //https://pub.dev/packages/flutter_map#-installing-tab-
@@ -15,9 +16,30 @@ class MapaPage extends StatelessWidget {
           IconButton(icon: Icon(Icons.my_location), onPressed: () {})
         ],
       ),
-      body: Center(
-        child: Text(scan.valor),
+      body: _crearFlutterMap(scan),
+    );
+  }
+
+  Widget _crearFlutterMap(ScanModel scan) {
+    return FlutterMap(
+      options: new MapOptions(
+        center: scan.getLatLng(),
+        zoom: 10.00
       ),
+      layers: [
+        _crearMapa(),
+      ],
+    );
+  }
+
+  _crearMapa() {
+    return TileLayerOptions(
+      urlTemplate: 'https://api.mapbox.com/v4/'
+          '{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}',
+      additionalOptions: {
+        'accessToken': 'pk.eyJ1IjoiYnJpYW52aCIsImEiOiJjazR3NTFkM3EwZWU0M2VxcXM0dnFidWtlIn0.E8WaxtdcIDhp6WY42iC7jA',
+        'id': 'mapbox.streets'
+      }
     );
   }
 }
